e13a502a83ac36e66b1857923a34c6392573e52f
cPLevey <d.levey@cpanel.net>
2017-06-15 11:13:15 -0500

    moved installer

--- installer.sh
+++ installer.sh
@@ -6,58 +6,61 @@
 #
 
 # Variables
+# If these are changed; the .plist file will need to be updated as well.
 
-launchagent_dir="~/.launchd";
+launchagent_dir="${HOME}/.launchd";
 logging_dir="$launchagent_dir/logs";
 
 # Check if homebrew is installed.
 
 if [ ! -e /usr/local/bin/brew ]; then 
-		echo -e "[ERROR] [$(date)]: Brew installation not found.";
+		printf "[ERROR] [$(date)]: Brew installation not found.\n";
 	else
 
 # Create necessary directories. 
 
 		if [ ! -d "$launchagent_dir" ]; then 
 			mkdir -p "$launchagent_dir"; 
-			echo -e "$launchagent_dir directory created.";
+			printf "$launchagent_dir directory created.\n";
 		fi
 
 		if [ ! -d "$logging_dir" ]; then 
 			mkdir -p "$logging_dir"; 
-			echo -e "$logging_dir directory created.";
+			printf "$logging_dir directory created.\n";
 		fi
 
 # Initiate log files.
 
 		if [ ! -f "$logging_dir/brewupdate.err" ]; then 
-			echo -e "Initiated brewupdate.err @ $(date)\n" >> "$logging_dir/brewupdate.err";
-			echo -e "Error log initiated.";
+			printf "Initiated brewupdate.err @ $(date)\n\n" >> "$logging_dir/brewupdate.err";
+			printf "Error log initiated.\n";
 		fi
 
 		if [ ! -f "$logging_dir/brewupdate.log" ]; then 
-			echo -e "Initiated brewupdate.log @ $(date)\n" >> "$logging_dir/brewupdate.log"
-			echo -e "Log file initiated.";
+			printf "Initiated brewupdate.log @ $(date)\n\n" >> "$logging_dir/brewupdate.log"
+			printf "Log file initiated.\n";
 		fi
 
 # Grab bash "cron job" and LaunchD plist file.
 
-		echo -e "Grabbing the goods.\n";
+		printf "Grabbing the goods.\n\n";
 			
-			wget -nv "https://raw.githubusercontent.com/cPLevey/macOS-brewupdate-launchd/master/.launchd/brewupdate.sh" -O "~/$launchagent_dir/brewupdate.sh";
-			wget -nv "https://raw.githubusercontent.com/cPLevey/macOS-brewupdate-launchd/master/.launchd/homebrew.brewupdate.plist" -O "~/$launchagent_dir/homebrew.brewupdate.plist";
+			wget -nv "https://raw.githubusercontent.com/cPLevey/macOS-brewupdate-launchd/master/.launchd/brewupdate.sh" -O $launchagent_dir/brewupdate.sh;
+				chmod +x $launchagent_dir/brewupdate.sh;
+			
+			wget -nv "https://raw.githubusercontent.com/cPLevey/macOS-brewupdate-launchd/master/.launchd/homebrew.brewupdate.plist" -O $launchagent_dir/homebrew.brewupdate.plist;
 
 # Run job for the firt time.
 
-		echo -e "Running job (without log redirect) for the first time.";
+		printf "Running job (without log redirect) for the first time.\n";
 
-			/bin/sh ~/$launchagent_dir/brewupdate.sh
+			/bin/sh $launchagent_dir/brewupdate.sh
 
 # Loading LaunchAgent plist.
 
-		launchctl load ~/$launchagent_dir/homebrew.brewupdate.plist
-			
+		launchctl load $launchagent_dir/homebrew.brewupdate.plist
+		launchctl list |grep "brewupdate";
 
-
+		printf "Setup completed.\n";
 
 fi
